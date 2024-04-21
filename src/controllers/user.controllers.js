7ebf8a7d3794aa444a541386cc5a63da8a56be24
KajalJadhao27 <kajaljadhao272@gmail.com>
2024-04-22 00:37:33 +0530

    fix all register controller bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,80 +1,91 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {User} from "../models/user.model.js"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
-const registerUser = asyncHandler( async (req, res) => {
- //get details of user from frontend
 
- //validation-not empty
+const registerUser = asyncHandler(async (req, res) => {
+  // Get details of user from frontend
 
- //check if user already exists:username,email
+  // Validation - not empty
 
- //check for images ,check for avatar
+  // Check if user already exists: username, email
 
- //upload them to cloudinary,avtar
+  // Check for images, check for avatar
 
- //create user object- create entry in db
+  // Upload them to cloudinary, avatar
 
- //remove password and refresh token field from response
+  // Create user object - create entry in db
 
- //check for user creation
+  // Remove password and refresh token field from response
 
- //return response
+  // Check for user creation
 
-const {fullName, email, username, password } = req.body
-console.log('email:', email);
+  // Return response
 
-if ([fullName, email, username, password].some((field) => field?.trim() === "")
-)
-{
-  throw new ApiError(400, "All field are required")
+  const { fullName, email, username, password } = req.body;
+  
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+) {
+    throw new ApiError(400, "All fields are required")
 }
 
-User.findOne({
-  $or: [{username}, {email}]
+
+const existedUser = await User.findOne({
+  $or: [{ username }, { email }]
 })
+
 if (existedUser) {
   throw new ApiError(409, "User with email or username already exists")
 }
 
 const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
-A
-if (!avatarLocalPath) {
-  throw new ApiError(400, "Avatar file is required")
-}
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-const avatar = await uploadOnCloudinary(avatarLocalPath)
-const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
 
-if (!avatar) {
-  throw new ApiError(400, "Avatar file is required")
-}
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
 
-const user = await User.create({
-  fullName,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username: username.toLowerCase()
-})
- 
-const createdUser = await User.findById(user._id).select(
-  "-password -refreshToken"
-)
-if (createdUser) {
-  throw new ApiError(500, "Something went wrong while registering the user")
-}
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  console.log('Avatar upload result:', avatar);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-return res.status(201).json(
-  new ApiResponse(200, createdUser, "User register successfully")
-)
 
-})
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file upload failed");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  });
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  );
+});
 
 export {
   registerUser,
-}
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import { Jwt } from "jsonwebtoken";
+import  Jwt  from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
@@ -77,7 +77,7 @@ userSchema.methods.generateAccessToken = function(){
 }
 
 userSchema.methods.generateRefreshToken = function(){
-  return jwt.sign(
+  return Jwt.sign(
     {
       _id: this._id,
       email: this.email,
